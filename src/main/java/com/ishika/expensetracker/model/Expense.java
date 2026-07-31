c8b63d9fde4855ab0914773087f7473a30cfa39f
Ishika-45 <bansalishika58@gmail.com>
2026-08-01 00:21:38 +0530

    Add Expense model

---
+++ src/main/java/com/ishika/expensetracker/model/Expense.java
@@ -0,0 +1,64 @@
+package com.ishika.expensetracker.model;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Expense {
+    private UUID id;
+    private String title;
+    private BigDecimal amount;
+    private String category;
+    private LocalDate date;
+
+    // Constructors
+    public Expense() {}
+
+    public Expense(UUID id, String title, BigDecimal amount, String category, LocalDate date) {
+        this.id = id;
+        this.title = title;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+    }
+
+    // Getters and Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
